03a7fbae60d39c38f3cff02b1d2242dc313548ec
Emilio Palumbo <emiliopalumbo@gmail.com>
2018-02-14 17:02:24 +0100

    Update table rendering using JSON data

--- src/main/resources/nextflow/trace/assets/ReportTemplate.js
+++ src/main/resources/nextflow/trace/assets/ReportTemplate.js
@@ -153,113 +153,128 @@ $(function() {
   }
   function make_tasks_table(){
     // reset
-    if ( $.fn.dataTable.isDataTable( '#tasks_table' ) ) {
-      $('#tasks_table').DataTable().destroy();
-    }
-    $('#tasks_table tbody').html('');
-    // Add rows
-    for(i=0; i<window.data['trace'].length; i++){
-      // Use a nice label for the status
-      var status = window.data['trace'][i]['status'];
-      if(status == 'COMPLETED'){ status = '<span class="badge badge-success">COMPLETED</span>'; }
-      if(status == 'CACHED'){ status = '<span class="badge badge-secondary">CACHED</span>'; }
-      if(status == 'ABORTED'){ status = '<span class="badge badge-danger">ABORTED</span>'; }
-      if(status == 'FAILED'){ status = '<span class="badge badge-danger">FAILED</span>'; }
-      // Trim whitespace properly from the script
-      var script = '';
-      var lines = window.data['trace'][i]['script'].split("\n");
-      var ws_re = /^(\s+)/g;
-      var flws_match = ws_re.exec(lines[1]);
-      if(flws_match == null){
-        script = window.data['trace'][i]['script'];
-      } else {
-        for(var j=0; j<lines.length; j++){
-          script += lines[j].replace(new RegExp('^'+flws_match[1]), '').replace(/\s+$/,'') + "\n";
-        }
+      if ( $.fn.dataTable.isDataTable( '#tasks_table' ) ) {
+        $('#tasks_table').DataTable().destroy();
       }
-      $('#tasks_table tbody').append('<tr>'+
-        '<td>' + window.data['trace'][i]['task_id'] + '</td>'+
-        '<td>' + window.data['trace'][i]['process'] + '</td>'+
-        '<td>' + window.data['trace'][i]['tag'] + '</td>'+
-        '<td>' + status + '</td>'+
-        '<td><code>' + window.data['trace'][i]['hash'] + '</code></td>'+
-        '<td>' + window.data['trace'][i]['cpus'] + '</td>'+
-        '<td>' + window.data['trace'][i]['%cpu'] + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['memory']) + '</td>'+
-        '<td>' + window.data['trace'][i]['%mem'] + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['vmem']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['rss']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['peak_vmem']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['peak_rss']) + '</td>'+
-        '<td>' + make_duration(window.data['trace'][i]['time']) + '</td>'+
-        '<td>' + make_duration(window.data['trace'][i]['duration']) + '</td>'+
-        '<td>' + make_duration(window.data['trace'][i]['realtime']) + '</td>'+
-        '<td><pre class="script_block short"><code>' + script.trim() + '</code></pre></td>'+
-        '<td>' + window.data['trace'][i]['exit'] + '</td>'+
-        '<td>' + make_date(window.data['trace'][i]['submit']) + '</td>'+
-        '<td>' + make_date(window.data['trace'][i]['start']) + '</td>'+
-        '<td>' + make_date(window.data['trace'][i]['complete']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['rchar']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['wchar']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['syscr']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['syscw']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['read_bytes']) + '</td>'+
-        '<td>' + make_memory(window.data['trace'][i]['write_bytes']) + '</td>'+
-        '<td>' + window.data['trace'][i]['native_id'] + '</td>'+
-        '<td>' + window.data['trace'][i]['name'] + '</td>'+
-        '<td>' + window.data['trace'][i]['module'] + '</td>'+
-        '<td>' + window.data['trace'][i]['container'] + '</td>'+
-        '<td>' + window.data['trace'][i]['disk'] + '</td>'+
-        '<td>' + window.data['trace'][i]['attempt'] + '</td>'+
-        '<td><samp>' + window.data['trace'][i]['scratch'] + '</samp></td>'+
-        '<td><samp>' + window.data['trace'][i]['workdir'] + '</samp></td>'+
-      +'</tr>');
+      var table = $('#tasks_table').DataTable({
+        data: window.data.trace,
+        columns: [
+          { title: 'task_id', data: 'task_id' },
+          { title: 'process', data: 'process' },
+          { title: 'tag', data: 'tag' },
+          { title: 'status', data: 'status', render: function(data, type, row){
+              var s = {
+                COMPLETED: 'success',
+                CACHED: 'secondary',
+                ABORTED: 'danger',
+                FAILED: 'danger'
+              }
+              return '<span class="badge badge-'+s[data]+'">'+data+'</span>';
+            }
+          },
+          { title: 'hash', data: 'hash', render:  function(data, type, row){
+              var script = '';
+              var lines = data.split("\n");
+              var ws_re = /^(\s+)/g;
+              var flws_match = ws_re.exec(lines[1]);
+              if(flws_match == null){
+                script = data;
+              } else {
+                for(var j=0; j<lines.length; j++){
+                  script += lines[j].replace(new RegExp('^'+flws_match[1]), '').replace(/\s+$/,'') + "\n";
+                }
+              }
+              return '<code>'+script+'</code>';
+            }
+          },
+          { title: 'allocated cpus', data: 'cpus' },
+          { title: '%cpu', data: '%cpu' },
+          { title: 'allocated memory', data: 'memory', render: make_memory },
+          { title: '%mem', data: '%mem' },
+          { title: 'vmem', data: 'vmem', render: make_memory },
+          { title: 'rss', data: 'rss', render: make_memory },
+          { title: 'peak_vmem', data: 'peak_vmem', render: make_memory },
+          { title: 'peak_rss', data: 'peak_rss', render: make_memory },
+          { title: 'allocated time', data: 'time', render: make_duration },
+          { title: 'duration', data: 'duration', render: make_duration },
+          { title: 'realtime', data: 'realtime', render: make_duration },
+          { title: 'script', data: 'script', render: function(data) {
+              return '<pre class="script_block short"><code>' + data.trim() + '</code></pre>';
+            }
+          },
+          { title: 'exit', data: 'exit' },
+          { title: 'submit', data: 'submit', render: make_date },
+          { title: 'start', data: 'start', render: make_date },
+          { title: 'complete', data: 'complete', render: make_date },
+          { title: 'rchar', data: 'rchar', render: make_memory },
+          { title: 'wchar', data: 'wchar', render: make_memory },
+          { title: 'syscr', data: 'syscr', render: make_memory },
+          { title: 'syscw', data: 'syscw', render: make_memory },
+          { title: 'read_bytes', data: 'read_bytes', render: make_memory },
+          { title: 'write_bytes', data: 'write_bytes', render: make_memory },
+          { title: 'native_id', data: 'native_id' },
+          { title: 'name', data: 'name' },
+          { title: 'module', data: 'module' },
+          { title: 'container', data: 'container', render: function(data) {
+              return '<samp>'+data+'</samp>';
+            }
+          },
+          { title: 'disk', data: 'disk' },
+          { title: 'attempt', data: 'attempt' },
+          { title: 'scratch', data: 'scratch', render: function(data) {
+              return '<samp>'+data+'</samp>';
+            }
+          },
+          { title: 'workdir', data: 'workdir', render: function(data) {
+              return '<samp>'+data+'</samp>';
+            }
+          }
+        ],
+        "deferRender": true,
+        "lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
+        "scrollX": true,
+        "colReorder": true,
+        "columnDefs": [
+          { className: "id", "targets": [ 0,1,2,3 ] },
+          { className: "meta", "targets": [ 4,13,16,17,18,19,20,27,28,29,30,31,32,33,34 ] },
+          { className: "metrics", "targets": [ 5,6,7,8,9,10,11,12,14,15,21,22,23,24,25,26 ] }
+        ],
+        "buttons": [
+          {
+            extend: 'colvisGroup',
+            text: 'Metrics',
+            show: [ '.id', '.metrics' ],
+            hide: [ '.meta' ],
+          },
+          {
+            extend: 'colvisGroup',
+            text: 'Metadata',
+            show: [ '.id', '.meta'],
+            hide: [ '.metrics' ],
+          },
+          {
+            extend: 'colvisGroup',
+            text: 'All',
+            show: ':hidden',
+          },
+        ]
+      });
+
+      // Insert column filter button group
+      table.buttons().container()
+         .prependTo( $('#tasks_table_filter') );
+
+      // Column filter button group onClick event to highlight active filter
+      $('.buttons-colvisGroup').click(function(){
+        var def = 'btn-secondary';
+        var sel = 'btn-primary';
+        $('.buttons-colvisGroup').removeClass(sel).addClass(def);
+        $(this).addClass(sel).removeClass(def);
+      });
+
+      // Default filter highlight
+      $(".buttons-colvisGroup:contains('All')").click();
     }
-    var table = $('#tasks_table').removeAttr('width').DataTable({
-      "lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
-      "scrollX": true,
-      "colReorder": true,
-      "columnDefs": [
-        { className: "id", "targets": [ 0,1,2,3 ] },
-        { className: "meta", "targets": [ 4,13,16,17,18,19,20,27,28,29,30,31,32,33,34 ] },
-        { className: "metrics", "targets": [ 5,6,7,8,9,10,11,12,14,15,21,22,23,24,25,26 ] }
-      ],
-      "buttons": [
-        {
-          extend: 'colvisGroup',
-          text: 'Metrics',
-          show: [ '.id', '.metrics' ],
-          hide: [ '.meta' ],
-        },
-        {
-          extend: 'colvisGroup',
-          text: 'Metadata',
-          show: [ '.id', '.meta'],
-          hide: [ '.metrics' ],
-        },
-        {
-          extend: 'colvisGroup',
-          text: 'All',
-          show: ':hidden',
-        },
-      ]
-    });
-
-    // Insert column filter button group
-    table.buttons().container()
-       .prependTo( $('#tasks_table_filter') );
-
-    // Column filter button group onClick event to highlight active filter
-    $('.buttons-colvisGroup').click(function(){
-      var def = 'btn-secondary';
-      var sel = 'btn-primary';
-      $('.buttons-colvisGroup').removeClass(sel).addClass(def);
-      $(this).addClass(sel).removeClass(def);
-    });
-
-    // Default filter highlight
-    $(".buttons-colvisGroup:contains('All')").click();
-  }
 
   // Dropdown changed about raw / human readable values in table
   $('#nf-table-humanreadable').change(function(){
